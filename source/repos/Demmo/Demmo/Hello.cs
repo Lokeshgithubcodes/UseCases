5e0a9211a6e6a64db440a8b8aedef4bcf5e8faa1
LOKESH R <lokesh46386@gmail.com>
2024-01-18 16:33:02 +0530

    first commit

---
+++ source/repos/Demmo/Demmo/Hello.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Demmo
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, res;
+            Console.WriteLine("Enter the number1");
+            num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the number2");
+            num2=int.Parse(Console.ReadLine());
+            res=num1 + num2;
+            Console.WriteLine(res);
+
+        }
+    }
+}
---
+++ source/repos/Demmo/Demmo/Hi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demmo
+{
+    public class Hi
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
