06cf06b2a6a53be1bc6dd26dbcc914a27bac5dff
LOKESH R <lokesh46386@gmail.com>
2024-01-18 16:10:11 +0530

    yes

---
+++ source/repos/AddressBookProject/AddressBookProject/AddressBook.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProject
+{
+    class AddressBook
+    {
+        private List<Contact> contacts = new List<Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public void DisplayContacts()
+        {
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"Name: {contact.FirstName} {contact.LastName}, Phone: {contact.PhoneNumber}");
+            }
+        }
+        public Contact FindContactByName(string firstName, string lastName)
+        {
+            return contacts.Find(contact => contact.FirstName.Equals(firstName, StringComparison.OrdinalIgnoreCase) && contact.LastName.Equals(lastName, StringComparison.OrdinalIgnoreCase));
+        }
+        public void DeleteContact(string firstName, string lastName)
+        {
+            Contact contactToDelete = FindContactByName(firstName, lastName);
+
+            if (contactToDelete != null)
+            {
+                contacts.Remove(contactToDelete);
+                Console.WriteLine($"Contact {contactToDelete.FirstName} {contactToDelete.LastName} deleted successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Contact not found.");
+            }
+        }
+        
+
+        public void EditContact(string firstName, string lastName)
+        {
+            Contact existingContact = FindContactByName(firstName, lastName);
+
+            if (existingContact != null)
+            {
+                Console.WriteLine($"Editing contact: {existingContact.FirstName} {existingContact.LastName}");
+
+                Console.WriteLine("Enter new Firstname");
+                existingContact.FirstName = Console.ReadLine();
+
+                Console.WriteLine("Enter new last name");
+                existingContact.LastName = Console.ReadLine();
+
+                Console.WriteLine("Enter new address");
+                existingContact.Address = Console.ReadLine();
+
+                Console.WriteLine("Enter new city name");
+                existingContact.City = Console.ReadLine();
+
+                Console.WriteLine("Enter new state name");
+                existingContact.State = Console.ReadLine();
+
+                Console.WriteLine("Enter new zip code");
+                existingContact.Zip = Console.ReadLine();
+
+                Console.WriteLine("Enter new phone number");
+                existingContact.PhoneNumber = Console.ReadLine();
+
+                Console.WriteLine("Enter new email id");
+                existingContact.Email = Console.ReadLine();
+
+                Console.WriteLine("Contact updated successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Contact not found.");
+            }
+        }
+
+    }
+}
---
+++ source/repos/AddressBookProject/AddressBookProject/Program.cs
@@ -0,0 +1,73 @@
+﻿using AddressBookProject;
+using System;
+namespace Demo
+{
+    class AddressBookMain
+    {
+        
+
+        static void Main(string[] args)
+        {
+           
+            //use Case1
+            AddressBook addressBook = new AddressBook();
+
+            Console.WriteLine("Enter Firstname");
+            string first = Console.ReadLine();
+
+            Console.WriteLine("Enter last name");
+            string last = Console.ReadLine();
+
+            Console.WriteLine("Enter the address");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("Enter city name");
+            string city = Console.ReadLine();
+
+            Console.WriteLine("Enter state name");
+            string state = Console.ReadLine();
+
+            Console.WriteLine("Enter zip code");
+            string zip = Console.ReadLine();
+
+            Console.WriteLine("Enter phone number");
+            string phone = Console.ReadLine();
+
+            Console.WriteLine("Enter email id");
+            string email = Console.ReadLine();
+
+            //Use Case2
+            Contact newContact1 = new Contact(first, last, address, city, state, zip, phone, email);
+            Contact newContact2 = new Contact(first, last, address, city, state, zip, phone, email);
+            addressBook.AddContact(newContact1);
+            addressBook.AddContact(newContact2);
+
+
+            //Use case3
+            Console.WriteLine("Enter the Firstname of the contact to edit:");
+            string editFirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter the last name of the contact to edit:");
+            string editLastName = Console.ReadLine();
+
+            addressBook.EditContact(editFirstName, editLastName);
+
+            //Use Case4
+
+            Console.WriteLine("Enter the Firstname of the contact to delete:");
+            string deleteFirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter the last name of the contact to delete:");
+            string deleteLastName = Console.ReadLine();
+
+            addressBook.DeleteContact(deleteFirstName, deleteLastName);
+
+
+
+            addressBook.DisplayContacts();
+        }
+
+        
+    }
+
+}
